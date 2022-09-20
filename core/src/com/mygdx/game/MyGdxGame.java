b5212ab4ee8cfd75d5373677d7d2b83cbd81d6b6
GamesGDX <skakunovmy@gmail.com>
2022-09-20 14:29:42 +0300

    Commit_03

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -6,12 +6,23 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
@@ -24,23 +35,98 @@ public class MyGdxGame extends ApplicationAdapter {
 	private Music music;
 	private Sound sound;
 
-	MyInputProcessor myInputProcessor;
+	private MyInputProcessor myInputProcessor;
+
+	private OrthographicCamera camera;
 	private float x, y;
 
-	private Rectangle rectangle, window;
 	private int dir = 0, step = 1;
+	private Rectangle rectangle, window;
+
+	private PhisX phisX;
+
+	private Body body;
+	private TiledMap map;
+	private OrthogonalTiledMapRenderer mapRenderer;
 
 	@Override
 	public void create () {
-		shapeRenderer = new ShapeRenderer();
+		map = new TmxMapLoader().load("map/безымянный.tmx");
+		mapRenderer = new OrthogonalTiledMapRenderer(map);
+
+		phisX = new PhisX();
+
+		BodyDef def = new BodyDef();
+		FixtureDef fDef = new FixtureDef();
+		PolygonShape shape = new PolygonShape();
+
+//		def.gravityScale = 1;
+		def.type = BodyDef.BodyType.StaticBody;
+//		def.position.set(0, 0);
+
+//		shape.setAsBox(250, 10);
+		fDef.shape = shape;
+		fDef.density = 1;
+		fDef.friction = 0;
+		fDef.restitution = 1;
+
+		MapLayer env = map.getLayers().get("env");
+		Array<RectangleMapObject> rect = env.getObjects().getByType(RectangleMapObject.class);
+		for (int i = 0; i < rect.size; i++) {
+			float x = rect.get(i).getRectangle().x;
+			float y = rect.get(i).getRectangle().y;
+			float w = rect.get(i).getRectangle().width/2;
+			float h = rect.get(i).getRectangle().height/2;
+			def.position.set(x, y);
+			shape.setAsBox(w, h);
+			phisX.world.createBody(def).createFixture(fDef).setUserData("Kubik");
+		}
+
+
+		def.type = BodyDef.BodyType.DynamicBody;
+		def.gravityScale = 4;
+		env = map.getLayers().get("dyn");
+		rect = env.getObjects().getByType(RectangleMapObject.class);
+		for (int i = 0; i < rect.size; i++) {
+			float x = rect.get(i).getRectangle().x;
+			float y = rect.get(i).getRectangle().y;
+			float w = rect.get(i).getRectangle().width/2;
+			float h = rect.get(i).getRectangle().height/2;
+			def.position.set(x, y);
+			shape.setAsBox(w, h);
+			fDef.shape = shape;
+			fDef.density = 1;
+			fDef.friction = 0;
+			fDef.restitution = 1;
+			phisX.world.createBody(def).createFixture(fDef).setUserData("Kubik");
+		}
+
+		env = map.getLayers().get("hero");
+		RectangleMapObject hero = (RectangleMapObject) env.getObjects().get("Hero");
+			float x = hero.getRectangle().x;
+			float y = hero.getRectangle().y;
+			float w = hero.getRectangle().width/2;
+			float h = hero.getRectangle().height/2;
+			def.position.set(x, y);
+			shape.setAsBox(w, h);
+			fDef.shape = shape;
+			fDef.density = 1;
+			fDef.friction = 0;
+			fDef.restitution = 1;
+		body = phisX.world.createBody(def);
+		body.createFixture(fDef).setUserData("Kubik");
+
+
+		shape.dispose();
+
+//		shapeRenderer = new ShapeRenderer();
 		rectangle = new Rectangle();
 		window = new Rectangle(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		myInputProcessor = new MyInputProcessor();
 		Gdx.input.setInputProcessor(myInputProcessor);
 
 		music = Gdx.audio.newMusic(Gdx.files.internal("48bb90af8e1e401.mp3"));
-		music.setVolume(0.5f);
-//		music.setPan(0, 1);
+		music.setVolume(0.25f);
 		music.setLooping(true);
 		music.play();
 
@@ -53,17 +139,24 @@ public class MyGdxGame extends ApplicationAdapter {
 		stand = new MyAtlasAnim("atlas/unnamed.atlas", "mario_stand", 10, Animation.PlayMode.LOOP);
 		jump = new MyAtlasAnim("atlas/unnamed.atlas", "mario_jump", 10, Animation.PlayMode.LOOP);
 
+		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 1, 1, 1);
+		ScreenUtils.clear(Color.BLACK);
+
+		camera.position.x = body.getPosition().x;
+		camera.position.y = body.getPosition().y;
+		camera.zoom = 0.5f;
+		camera.update();
+
+		mapRenderer.setView(camera);
+		mapRenderer.render();
 
 		tmpA = stand;
 		dir = 0;
-
-//		long x = Gdx.input.getX() - anim.draw().getRegionWidth()/2;
-//		long y = Gdx.graphics.getHeight() - Gdx.input.getY() - anim.draw().getRegionHeight()/8;
 
 		if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) sound.play(1, 1, 1);
 
@@ -77,9 +170,9 @@ public class MyGdxGame extends ApplicationAdapter {
 			dir = 1;
 			tmpA = run;
 		}
-		if(myInputProcessor.getOutString().contains("Up")) y++;
-		tmpA = jump;
-		if(myInputProcessor.getOutString().contains("Down")) y--;
+		if(myInputProcessor.getOutString().contains("Up")) { y++;
+		tmpA = jump;}
+		if(myInputProcessor.getOutString().contains("Down")) { y--; }
 		if(myInputProcessor.getOutString().contains("Space")) {
 			x = Gdx.graphics.getWidth()/2;
 			y = Gdx.graphics.getHeight()/2;
@@ -100,11 +193,12 @@ public class MyGdxGame extends ApplicationAdapter {
 
 		System.out.println(myInputProcessor.getOutString());
 
+		float x = body.getPosition().x - 2.5f;
+		float y = body.getPosition().y - 2.5f;
+
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(tmpA.draw(), x, y);
-//		batch.draw(img, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-//		batch.draw(png, x, y, png.getHeight()/2, png.getHeight()/2,	png.getWidth(), png.getHeight(), 1, 1, 0, 0, 0,
-//		png.getWidth(), png.getHeight(), false, false);
+		batch.draw(tmpA.draw(), body.getPosition().x, body.getPosition().y);
 		batch.end();
 
 		shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
@@ -116,14 +210,27 @@ public class MyGdxGame extends ApplicationAdapter {
 		window.overlaps(rectangle);
 		if (!window.contains(rectangle)) Gdx.graphics.setTitle("Out");
 		else Gdx.graphics.setTitle("In");
+
+//		phisX.step();
+		phisX.debugDraw(camera);
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		camera.viewportHeight = height;
+		camera.viewportWidth = width;
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
 		tmpA.dispose();
+		img.dispose();
+		stand.dispose();
 		music.dispose();
 		sound.dispose();
+		map.dispose();
+		mapRenderer.dispose();
 
 	}
 }
---
+++ core/src/com/mygdx/game/PhisX.java
@@ -0,0 +1,31 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class PhisX {
+    final World world;
+
+    private final Box2DDebugRenderer debugRenderer;
+
+    public PhisX() {
+        world = new World(new Vector2(0, -9.81f), true);
+        debugRenderer = new Box2DDebugRenderer();
+    }
+
+    public void debugDraw(OrthographicCamera camera) {
+        debugRenderer.render(world, camera.combined);
+    }
+
+    public void step() {
+    world.step(1/60f, 3, 3);
+    }
+
+    public void dispose() {
+        world.dispose();
+        debugRenderer.dispose();
+    }
+
+}
